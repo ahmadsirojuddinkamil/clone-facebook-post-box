972df956d3d91441932f09f21aa1fb85f2abc01c
agungzxx <agungiman98@gmail.com>
2023-02-11 20:37:15 +0800

    Styling Css
    Styling CSS with Atlantis Template of bootstrap

---
+++ Modules/Chat/Entities/User.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Modules\Chat\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class User extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+    
+    
+}
